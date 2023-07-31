f82246fe7ece1525e1f11bb1cbee169a85aca93c
0xLightt <0xlightt@protonmail.com>
2023-07-31 16:28:53 +0100

    Address #268

--- src/maia/tokens/ERC4626PartnerManager.sol
+++ src/maia/tokens/ERC4626PartnerManager.sol
@@ -323,12 +323,11 @@ abstract contract ERC4626PartnerManager is PartnerUtilityManager, Ownable, ERC46
     }
 
     modifier checkTransfer(address from, uint256 amount) virtual {
-        uint256 userBalance = balanceOf[from] * bHermesRate;
+        uint256 userBalance = (balanceOf[from] - amount) * bHermesRate;
 
         if (
-            userBalance - userClaimedWeight[from] < amount || userBalance - userClaimedBoost[from] < amount
-                || userBalance - userClaimedGovernance[from] < amount
-                || userBalance - userClaimedPartnerGovernance[from] < amount
+            userBalance < userClaimedWeight[from] || userBalance < userClaimedBoost[from] 
+                || userBalance < userClaimedGovernance[from]  || userBalance <  userClaimedPartnerGovernance[from] 
         ) revert InsufficientUnderlying();
 
         _;
